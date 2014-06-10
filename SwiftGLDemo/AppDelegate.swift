e5384ce4f6d0aba9a6ff0620dd5326fbca1d5c93
Scott Bennett <scottbennett912@gmail.com>
2014-06-09 20:47:57 -0500

    Initial Commit

---
+++ SwiftGLDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftGLDemo
+//
+//  Created by Scott Bennett on 2014-06-09.
+//  Copyright (c) 2014 Scott Bennett. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
