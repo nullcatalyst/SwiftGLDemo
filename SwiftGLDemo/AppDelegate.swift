ea90a65600bde1c7aff028c82a77f74e8c65106e
Scott Bennett <scottbennett912@gmail.com>
2014-06-09 20:50:42 -0500

    Basic non-OpenGL app

--- SwiftGLDemo/AppDelegate.swift
+++ SwiftGLDemo/AppDelegate.swift
@@ -9,18 +9,18 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
     @IBOutlet var window: NSWindow
-
-
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication!) -> Bool {
+        return true
+    }
 }
-
